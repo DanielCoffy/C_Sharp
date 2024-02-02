74b6f101b8bdb7ef746d31566db5bdb1991049cd
DanielCoffy <daniel.danielcoffy@gmail.com>
2024-02-01 23:26:56 -0300

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0; int b=1; int c=0;
+
+            Console.Write("Quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequencia de Fibonacci com " + valores + " valores");
+
+            for (int i = 0; i < valores; i++) 
+            {
+                if (i<valores-1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else 
+                {
+                    Console.Write(a);
+                }
+
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
