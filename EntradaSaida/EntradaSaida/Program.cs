0636b0b067f2d04838c813f50097c97b716657da
DanielCoffy <daniel.danielcoffy@gmail.com>
2024-02-01 23:28:00 -0300

    Add files via upload

---
+++ EntradaSaida/EntradaSaida/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Console.WriteLine("Daniel Trindade Coffy");//método que mostra na tele e pula a linha
+            Console.Write("Coffy"); //método que mostra na tela mas não pula a linha
+            */
+
+           // int codigo= Console.Read();// pegando o código da tecla EX.: quando digitado"a" aparece o código "97"
+           // Console.WriteLine(codigo);
+
+             string texto=Console.ReadLine();
+            Console.WriteLine(texto);
+
+
+            Console.ReadKey(); //aguarda digitarmos uma tecla
+        }
+    }
+}
